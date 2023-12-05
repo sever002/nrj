f5c8026c765fcfd971543f78d08c4cd8546248ab
sever002 <152936231+sever002@users.noreply.github.com>
2023-12-05 02:03:12 +0100

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,31 @@
+
+<?php 
+class conf { 
+   
+   public $url="";
+   public $admin="";
+   public $lang='en';//<-  ar en fr it es de
+
+    public $token='6323003361:AAGehGHirREnHIq9K93VS5ahT66k2Rh_RjA';
+    public $chatId='@PSP0aO0N0P0R0I0X0009988';
+
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
